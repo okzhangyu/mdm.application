07843fd7ade2685b482c6595e783be81a1c01434
fanxing <fanxing@EFED>
2018-11-05 10:36:35 +0800

    备份视图及存储过程

--- mdm-model/src/main/resources/sql/Account.sql
+++
@@ -1,5 +0,0 @@
-create view AVA_MDM_VIEW_OACT as
-select "AcctCode","AcctName","Finanse","Postable","Levels",
-"LocManTran","ActCurr","FatherNum","GrpLine" ,"ActType","CfwRlvnt",
-"AccntntCod"
-from OACT
--- mdm-model/src/main/resources/sql/OA_TASK.sql
+++ mdm-model/src/main/resources/sql/OA_TASK.sql
@@ -13,10 +13,8 @@ Create Procedure "AVA_SP_OA_TASKS"(
 	Declare Objtype varchar(30);
 	Declare Autokey int;
 	AutoKey := 1;
-	--select (select top 1 t1."CODE"    from CINF t0
-	--inner join OA_MID_BASE_TEST.AVA_OA_COMPANY t1 on t0."CompnyName" = t1."NAME") as code into CompanyName from dummy
 
-	select top 1 t1."CODE" into CompanyName   from CINF t0
+	select top 1 t1."CODE"  from CINF t0
 	inner join OA_MID_BASE_TEST.AVA_OA_COMPANY t1 on t0."CompnyName" = t1."NAME";
 
 	SELECT COUNT(*) INTO Flag FROM OA_MID_BASE_TEST."AVA_OA_TASK";
@@ -26,6 +24,38 @@ Create Procedure "AVA_SP_OA_TASKS"(
 	END IF;
 	-------------------单据添加--------------------------------
 	IF :transaction_type = N'A' THEN
+	--员工主数据
+	IF  :object_type=N'171' then
+
+		insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
+			select ROW_NUMBER() Over(Order by "empID") + AutoKey,CompanyName,"empID",'171','A'
+				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
+			FROM OHEM Where "empID" =  :list_of_cols_val_tab_del ;
+				SELECT MAX(DocEntry) INTO AutoKey FROM OA_MID_BASE_TEST."AVA_OA_TASK";
+				END IF;
+	IF  :object_type=N'119' then
+
+		insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
+			select ROW_NUMBER() Over(Order by "Code") + AutoKey,CompanyName,"Code",'119','A'
+				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
+			FROM OUDP Where "Code" =  :list_of_cols_val_tab_del ;
+
+				END IF;
+	--销售订单
+	IF  :object_type=N'17' then
+
+		insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
+			select ROW_NUMBER() Over(Order by "U_HTH") + AutoKey,CompanyName,"U_HTH",'63','A'
+				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
+			FROM ORDR Where "DocEntry" =  :list_of_cols_val_tab_del ;
+				SELECT MAX(DocEntry) INTO AutoKey FROM OA_MID_BASE_TEST."AVA_OA_TASK";
+
+			insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
+			select ROW_NUMBER() Over(Order by "U_GDH") + AutoKey,CompanyName,"U_GDH",'63','A'
+				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
+				FROM ORDR T0 INNER JOIN RDR1 T1 ON T0."DocEntry"=T1."DocEntry" Where T0."DocEntry" =  :list_of_cols_val_tab_del ;
+
+		end if;
 		--科目
 		IF :object_type = N'1' THEN
 			insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
@@ -48,26 +78,11 @@ Create Procedure "AVA_SP_OA_TASKS"(
 				FROM OCFW Where "CFWId" =  :list_of_cols_val_tab_del ;
 		End IF;
 		--成本中心
-		IF :object_type = N'61' THEN
+		IF :object_type = N'540000042' THEN
 			insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-			select ROW_NUMBER() Over(Order by "PrcCode") + AutoKey,CompanyName,"PrcCode",'61','A'
+			select ROW_NUMBER() Over(Order by "PrcCode") + AutoKey,CompanyName,"PrcCode",'540000042','A'
 				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
 				FROM OPRC Where "PrcCode" =  :list_of_cols_val_tab_del ;
-		End IF;
-		-- 部门
-		IF :object_type = N'119' THEN
-			insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-			select ROW_NUMBER() Over(Order by "Code") + AutoKey,CompanyName,"Code",'119','A'
-				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
-				FROM OUDP Where "Code" =  :list_of_cols_val_tab_del ;
-		End IF;
-
-		-- 人员
-		IF :object_type = N'171' THEN
-			insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-			select ROW_NUMBER() Over(Order by "empID") + AutoKey,CompanyName,"empID",'171','A'
-				,CURRENT_DATE,concat(hour(current_time),minute(current_time))
-				FROM OHEM Where "empID" =  :list_of_cols_val_tab_del ;
 		End IF;
 
 	END IF; -- INSERT
@@ -120,44 +135,16 @@ Create Procedure "AVA_SP_OA_TASKS"(
 
 		END IF;
 		--成本中心
-		IF :object_type = N'61'
+		IF :object_type = N'540000042'
 		THEN
 
 				insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-				select ROW_NUMBER() Over(Order by "PrcCode") + AutoKey,CompanyName,"PrcCode",'61','U'
+				select ROW_NUMBER() Over(Order by "PrcCode") + AutoKey,CompanyName,"PrcCode",'540000042','U'
 					,CURRENT_DATE,concat(hour(current_time),minute(current_time))
 					FROM OPRC Where "PrcCode" =  :list_of_cols_val_tab_del
 					and not exists(select UniqueKey from  OA_MID_BASE_TEST."AVA_OA_TASK" where CompanyCode = CompanyName
 																	and UniqueKey =  :list_of_cols_val_tab_del
-																	and ObjectCode = '61'
-																	and (IsSync = 'N' or IsSync = 'E'));
-
-		END IF;
-		--部门
-		IF :object_type = N'119'
-		THEN
-
-				insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-				select ROW_NUMBER() Over(Order by "Code") + AutoKey,CompanyName,"Code",'119','U'
-					,CURRENT_DATE,concat(hour(current_time),minute(current_time))
-					FROM OUDP Where "Code" =  :list_of_cols_val_tab_del
-					and not exists(select UniqueKey from  OA_MID_BASE_TEST."AVA_OA_TASK" where CompanyCode = CompanyName
-																	and UniqueKey =  :list_of_cols_val_tab_del
-																	and ObjectCode = '119'
-																	and (IsSync = 'N' or IsSync = 'E'));
-
-		END IF;
-		--人员
-		IF :object_type = N'171'
-		THEN
-
-				insert into  OA_MID_BASE_TEST."AVA_OA_TASK"(DocEntry,CompanyCode,UniqueKey,ObjectCode,OpType,CreateDate,CreateTime)
-				select ROW_NUMBER() Over(Order by "empID") + AutoKey,CompanyName,"empID",'171','U'
-					,CURRENT_DATE,concat(hour(current_time),minute(current_time))
-					FROM OHEM Where "empID" =  :list_of_cols_val_tab_del
-					and not exists(select UniqueKey from  OA_MID_BASE_TEST."AVA_OA_TASK" where CompanyCode = CompanyName
-																	and UniqueKey =  :list_of_cols_val_tab_del
-																	and ObjectCode = '171'
+																	and ObjectCode = '540000042'
 																	and (IsSync = 'N' or IsSync = 'E'));
 
 		END IF;
